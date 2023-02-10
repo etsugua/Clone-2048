00caa7dd2f313fefe151e726bef41af7c624158d
Auguste Cunha <auguste.cunha@gmail.com>
2023-02-10 15:20:57 +0000

    Moving pieces, kind of in the right direction, still broken

--- Assets/Scripts/Specific/Gameplay/GameBoard.cs
+++ Assets/Scripts/Specific/Gameplay/GameBoard.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Specific.Gameplay
 {
@@ -31,22 +32,179 @@ namespace Specific.Gameplay
             GenerateNewTile();
         }
 
-        public void PushTowardsDirection(Direction direction)
+        public async void PushTowardsDirection(Direction direction)
         {
             switch (direction)
             {
                 case Direction.Up:
-                    // TODO
+                    MovePiecesUp();
                     break;
                 case Direction.Down:
-                    // TODO
+                    MovePiecesDown();
                     break;
                 case Direction.Left:
-                    // TODO
+                    MovePiecesLeft();
                     break;
                 case Direction.Right:
-                    // TODO
+                    MovePiecesRight();
                     break;
+            }
+
+            await Task.Delay(1000);
+        }
+
+        private void MovePiecesUp()
+        {
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = board.GetLength(1) - 1; y >= 0; y--)
+                {
+                    // if the tile is not empty
+                    if (board[x, y].GetComponent<Tile>().Value != 0)
+                    {
+                        // loop through the tiles below it
+                        for (int i = y - 1; i >= 0; i--)
+                        {
+                            // if the tile is empty, move the piece down
+                            if (board[x, i].GetComponent<Tile>().Value == 0)
+                            {
+                                board[x, i].GetComponent<Tile>().Value = board[x, y].GetComponent<Tile>().Value;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                            }
+                            // if the tile is not empty, check if the value is the same
+                            else if (board[x, i].GetComponent<Tile>().Value == board[x, y].GetComponent<Tile>().Value)
+                            {
+                                // if the value is the same, merge the pieces
+                                board[x, i].GetComponent<Tile>().Value *= 2;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                                break;
+                            }
+                            // if the tile is not empty and the value is not the same, stop moving the piece
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void MovePiecesDown()
+        {
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    // if the tile is not empty
+                    if (board[x, y].GetComponent<Tile>().Value != 0)
+                    {
+                        // loop through the tiles above it
+                        for (int i = y + 1; i < board.GetLength(1); i++)
+                        {
+                            // if the tile is empty, move the piece up
+                            if (board[x, i].GetComponent<Tile>().Value == 0)
+                            {
+                                board[x, i].GetComponent<Tile>().Value = board[x, y].GetComponent<Tile>().Value;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                            }
+                            // if the tile is not empty, check if the value is the same
+                            else if (board[x, i].GetComponent<Tile>().Value == board[x, y].GetComponent<Tile>().Value)
+                            {
+                                // if the value is the same, merge the pieces
+                                board[x, i].GetComponent<Tile>().Value *= 2;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                                break;
+                            }
+                            // if the tile is not empty and the value is not the same, stop moving the piece
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void MovePiecesLeft()
+        {
+            // pieces should move to the left of the board, until hitting a piece or a wall
+            // if the piece hits a piece with the same value, merge the pieces
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                // loop through the board from top to bottom
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    // if the tile is not empty
+                    if (board[x, y].GetComponent<Tile>().Value != 0)
+                    {
+                        // loop through the tiles to the left of it
+                        for (int i = x - 1; i >= 0; i--)
+                        {
+                            // if the tile is empty, move the piece left
+                            if (board[i, y].GetComponent<Tile>().Value == 0)
+                            {
+                                board[i, y].GetComponent<Tile>().Value = board[x, y].GetComponent<Tile>().Value;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                            }
+                            // if the tile is not empty, check if the value is the same
+                            else if (board[i, y].GetComponent<Tile>().Value == board[x, y].GetComponent<Tile>().Value)
+                            {
+                                // if the value is the same, merge the pieces
+                                board[i, y].GetComponent<Tile>().Value *= 2;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                                break;
+                            }
+                            // if the tile is not empty and the value is not the same, stop moving the piece
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void MovePiecesRight()
+        {
+            // pieces move to the right of the board, until hitting a piece or a wall
+            // if the piece hits a piece with the same value, merge the pieces
+            
+            for (int x = board.GetLength(0) - 1; x >= 0; x--)
+            {
+                // loop through the board from top to bottom
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    // if the tile is not empty
+                    if (board[x, y].GetComponent<Tile>().Value != 0)
+                    {
+                        // loop through the tiles to the right of it
+                        for (int i = x + 1; i < board.GetLength(0); i++)
+                        {
+                            // if the tile is empty, move the piece right
+                            if (board[i, y].GetComponent<Tile>().Value == 0)
+                            {
+                                board[i, y].GetComponent<Tile>().Value = board[x, y].GetComponent<Tile>().Value;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                            }
+                            // if the tile is not empty, check if the value is the same
+                            else if (board[i, y].GetComponent<Tile>().Value == board[x, y].GetComponent<Tile>().Value)
+                            {
+                                // if the value is the same, merge the pieces
+                                board[i, y].GetComponent<Tile>().Value *= 2;
+                                board[x, y].GetComponent<Tile>().Value = 0;
+                                break;
+                            }
+                            // if the tile is not empty and the value is not the same, stop moving the piece
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
 
